920883db4465a00c2c0ce5e41700830167028df3
Ricy <m.j.hilhorst@students.uu.nl>
2019-04-17 19:05:36 +0200

    Small changes

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='albert',
-      version='1.3',
+      version='1.3.1',
       description='Interact with hidden REST API of albertheijn.nl',
       long_description = long_description,
       long_description_content_type="text/markdown",
-      url='https://github.com/6610749/albertheijn',
-      author='Michael Hilhorst',
+      url='https://github.com/MHilhorst/albertheijn',
+      author='M. Hilhorst',
       install_requires=[
           'requests'
       ],
